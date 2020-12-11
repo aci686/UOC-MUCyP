7c283dbc091e6d2b861e613cbcc267bd940637be
Aaron <56502347+aci686@users.noreply.github.com>
2020-12-11 23:20:38 +0100

    Create pec3.c

---
+++ M.1874/PEC 3/pec3.c
@@ -0,0 +1,16 @@
+/*
+__author__ = "Aaron Castro"
+__author_email__ = "aaron.castro.sanchez@outlook.com"
+__copyright__ = "Aaron Castro"
+__license__ = "MIT"
+*/
+
+#include <stdio.h>
+
+void main(void) {
+    char input_s[8];
+
+    puts("Insert master database password: ");
+    gets(input_s);
+    printf("Access granted!");
+}
