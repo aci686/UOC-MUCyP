0717b81834a23df7de2b4b8bd70e7f3c1aa46335
Aaron <56502347+aci686@users.noreply.github.com>
2021-10-12 16:47:04 +0200

    Proof of work algorithm

--- M.1875/PEC 1/Activity5_PEC1.py
+++ M.1875/PEC 1/Activity5_PEC1.py
@@ -2,13 +2,16 @@
 
 import sys
 # import SHA-256 hash function library
-
+from hashlib import sha256
+import binascii
+import time
+import struct
 
 # Version (4 bytes) - version = 1
-version =
+version = 1
 
 # Previous hash (32 bytes) - Double sha256 of your first name = sha256(sha256("firstname"))
-prev_hash =
+prev_hash = sha256(sha256('Aaron'.encode('utf-8')).digest()).digest()[::-1]
 
 # Merkle root (32 bytes)
 merkle_root = '6dbba50b72ad0569c2449090a371516e3865840e905483cac0f54d96944eee28'
@@ -20,25 +23,27 @@ bits = '1e0fffff'
 target = '00000fffff000000000000000000000000000000000000000000000000000000'
 
 # Nonce (4 bytes) - nonce = 0
-nonce =
+nonce = 0
 
-
-# Function - Convert transactions from big endian into little endian (hex)
-def big_endian_to_little_endian(transaction):
+# Function - Convert values from big endian into little endian (hex)
+def big_endian_to_little_endian(value):
     # CODE HERE
-
+    return(binascii.unhexlify(value)[::-1])
 
 # Function - PoW simulation
-def proof_of_work(version, prev_hash, merkle_root, target):
+# Don't forget to use the "big_endian_to_little_endian(value)" function!
+# Don't forget to save the nonce and timestamp values!
+def proof_of_work():
+    s_timestamp = time.time()
     # Iterate over the nonce to simulate real performance
-    for(...):
-        timestamp =
-        nonce =
-        # Header = {version, prev_hash, merkle_root, timestamp, bits, nonce}
-        header =
-        block_hash = sha256(sha256(header))
+    nonce = 0
+    for _ in iter(int, 1):
+        header = struct.Struct('<L').pack(version) + prev_hash + big_endian_to_little_endian(merkle_root.encode('utf-8')) + struct.Struct('<L').pack(int(s_timestamp)) + big_endian_to_little_endian(bits) + struct.Struct('<L').pack(nonce)
+        block_hash = sha256(header).digest()
         # Check if the block_hash is below the given target
-        if():
-            # return block_hash
+        if str(block_hash) < target:
+            return block_hash
+        nonce += 1
 
-proof_of_work()
+# Print block hash
+print(proof_of_work())
